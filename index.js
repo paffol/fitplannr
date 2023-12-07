81cb18aca37424425a9fe6b83524140e667780ce
baxu123 <152433527+baxu123@users.noreply.github.com>
2023-12-07 13:43:11 -0500

    start of planner.ejs

--- index.js
+++ index.js
@@ -28,41 +28,13 @@ app.get('/login', (req, res) => {
   //res.sendFile(path.resolve(__dirname,'public','workout.html'));
 });
 
-app.get('/signup', (req, res) => {
-    res.render('signup');
+app.get('/planner', (req, res) => {
+    res.render('planner');
    //res.sendFile(path.resolve(__dirname,'public','workout.html'));
  });
 
-app.post('/postal', (req, res) => {
-  const city = req.body.cityName;
-  const postal = req.body.postalName;
-  let number;
-  db.list().then(keys => {
-    number = keys.length;
-    const key = "postal"+number; 
-    const both = city + " " + postal;
-    add(key, both);
-  })
-})
-
 /* Useful Database method to  */
-function add(key, value){
-  db.set(key, value).then(() => {
-    printDB();
-  })
-}
 
-function printDB(){
-  db.list().then(keys => {
-    console.log("Database contents: ");
-    for(let i = 0; i < keys.length; i++){
-      db.get(keys[i]).then(value => {
-        console.log(keys[i] + ":" + value);
-      })
-    }
-  })
-  console.log();
-}
 
 app.listen(3000, () => {
   console.log('Express server initialized');

bcac2c8b77ddd608c62ab0e50bb2a9695e8b0316
paffol <98435500+paffol@users.noreply.github.com>
2023-12-07 14:26:51 -0500

    Added navbar functionality

--- index.js
+++ index.js
@@ -11,8 +11,28 @@ app.get('/', (req, res) => {
   res.render('index'); // Assuming you have an EJS file named 'index.ejs'
 });
 
+app.get('/calendar', (req, res) => {
+    res.render('calendar')
+  });
+
+app.get('/gyms', (req, res) => {
+    res.render('gyms')
+  });
+
 app.get('/login', (req, res) => {
+<<<<<<< Updated upstream
     res.render('login');
+=======
+    res.render('login')
+  });
+
+app.get('/nutrition', (req, res) => {
+    res.render('nutrition')
+  });
+
+app.get('/planner', (req, res) => {
+    res.render('planner')
+>>>>>>> Stashed changes
   });
 
 app.listen(port, () => {

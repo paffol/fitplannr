37dd32459f7bc7b0372dda47c5fb41260d0498ec
paffol <98435500+paffol@users.noreply.github.com>
2023-12-07 13:45:29 -0500

    Added express/ejs template

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Your routes and other middleware go here
+
+app.get('/', (req, res) => {
+  res.render('index'); // Assuming you have an EJS file named 'index.ejs'
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});

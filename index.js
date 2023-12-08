5614d90116e8ee5cce16eca2082f03e695b0c145
paffol <98435500+paffol@users.noreply.github.com>
2023-12-08 12:24:47 -0500

    Added styling/combined pages

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 3000;
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
 // Your routes and other middleware go here
 
 app.get('/', (req, res) => {
@@ -29,6 +31,10 @@ app.get('/nutrition', (req, res) => {
 
 app.get('/planner', (req, res) => {
     res.render('planner')
+  });
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
   });
 
 app.listen(port, () => {

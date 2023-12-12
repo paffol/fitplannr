d4df487f9fd2a127e551288c54cf91e40978d6d9
liudennis80 <152433269+liudennis80@users.noreply.github.com>
2023-12-12 13:10:08 -0500

    edit#2 add options f=to chose for breakfast lunch and dinner

--- script.js
+++ script.js
@@ -1,27 +1,85 @@
-let totalCalories = 0;
+let totalCaloriesBreakfast = 0;
+let totalCaloriesLunch = 0;
+let totalCaloriesDinner = 0;
 
-function addFood() {
-  const foodName = document.getElementById("foodName").value;
-  const calories = parseFloat(document.getElementById("calories").value);
+function changeTab(tabName) {
+  // Hide all tab content
+  document.querySelectorAll('.tab-content').forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  // Deactivate all tabs
+  document.querySelectorAll('.tabs button').forEach(tabButton => {
+    tabButton.classList.remove('active');
+  });
+
+  // Show the selected tab content and activate the button
+  document.getElementById(`${tabName}Tab`).classList.add('active');
+  document.querySelector(`.tabs button:contains('${tabName.charAt(0).toUpperCase() + tabName.slice(1)}')`).classList.add('active');
+}
+
+function changeDay() {
+  // Reset all data and switch to the Breakfast tab when the day changes
+  totalCaloriesBreakfast = 0;
+  totalCaloriesLunch = 0;
+  totalCaloriesDinner = 0;
+  updateTotalCalories('breakfast');
+  updateTotalCalories('lunch');
+  updateTotalCalories('dinner');
+  changeTab('breakfast');
+}
+
+function addFood(meal) {
+  const foodName = document.getElementById(`${meal}FoodName`).value;
+  const calories = parseFloat(document.getElementById(`${meal}Calories`).value);
 
   if (foodName && !isNaN(calories) && calories > 0) {
-    const foodList = document.getElementById("foodList");
-    const listItem = document.createElement("li");
-    listItem.textContent = `${foodName}: ${calories} calories`;
+    const foodList = document.getElementById(`${meal}List`);
+    const listItem = document.createElement("tr");
+    listItem.innerHTML = `<td>${foodName}</td><td>${calories}</td>`;
     foodList.appendChild(listItem);
 
-    totalCalories += calories;
-    updateTotalCalories();
-    
+    // Update total calories for the specific meal
+    if (meal === 'breakfast') {
+      totalCaloriesBreakfast += calories;
+      updateTotalCalories('breakfast');
+    } else if (meal === 'lunch') {
+      totalCaloriesLunch += calories;
+      updateTotalCalories('lunch');
+    } else if (meal === 'dinner') {
+      totalCaloriesDinner += calories;
+      updateTotalCalories('dinner');
+    }
+
     // Clear input fields
-    document.getElementById("foodName").value = "";
-    document.getElementById("calories").value = "";
+    document.getElementById(`${meal}FoodName`).value = "";
+    document.getElementById(`${meal}Calories`).value = "";
   } else {
     alert("Please enter valid values for food name and calories.");
   }
 }
 
-function updateTotalCalories() {
-  const totalCaloriesElement = document.getElementById("totalCalories");
-  totalCaloriesElement.textContent = `Total Calories: ${totalCalories}`;
+function updateTotalCalories(meal) {
+  const totalCaloriesElement = document.getElementById(`${meal}TotalCalories`);
+  let totalCalories = 0;
+
+  // Get the total calories for the specific meal
+  if (meal === 'breakfast') {
+    totalCalories = totalCaloriesBreakfast;
+  } else if (meal === 'lunch') {
+    totalCalories = totalCaloriesLunch;
+  } else if (meal === 'dinner') {
+    totalCalories = totalCaloriesDinner;
+  }
+
+  totalCaloriesElement.textContent = totalCalories;
 }
+
+// Helper function to simulate :contains selector
+function contains(selector, text) {
+  const elements = document.querySelectorAll(selector);
+  return Array.from(elements).find(el => el.textContent.includes(text));
+}
+
+// Set the initial state
+changeTab('breakfast');

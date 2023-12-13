82174815d1c37e8ce825a92580180b4704092a87
liudennis80 <152433269+liudennis80@users.noreply.github.com>
2023-12-13 14:15:56 -0500

    create lunch and dinner options

--- script.js
+++ script.js
@@ -3,23 +3,19 @@ let totalCaloriesLunch = 0;
 let totalCaloriesDinner = 0;
 
 function changeTab(tabName) {
-  // Hide all tab content
   document.querySelectorAll('.tab-content').forEach(tab => {
     tab.classList.remove('active');
   });
 
-  // Deactivate all tabs
   document.querySelectorAll('.tabs button').forEach(tabButton => {
     tabButton.classList.remove('active');
   });
 
-  // Show the selected tab content and activate the button
   document.getElementById(`${tabName}Tab`).classList.add('active');
-  contains('.tabs button', tabName.charAt(0).toUpperCase() + tabName.slice(1)).classList.add('active');
+  document.querySelector(`.tabs button:contains('${tabName.charAt(0).toUpperCase() + tabName.slice(1)}')`).classList.add('active');
 }
 
 function changeDay() {
-  // Reset all data and switch to the Breakfast tab when the day changes
   totalCaloriesBreakfast = 0;
   totalCaloriesLunch = 0;
   totalCaloriesDinner = 0;
@@ -39,7 +35,6 @@ function addFood(meal) {
     listItem.innerHTML = `<td>${foodName}</td><td>${calories}</td>`;
     foodList.appendChild(listItem);
 
-    // Update total calories for the specific meal
     if (meal === 'breakfast') {
       totalCaloriesBreakfast += calories;
       updateTotalCalories('breakfast');
@@ -51,7 +46,6 @@ function addFood(meal) {
       updateTotalCalories('dinner');
     }
 
-    // Clear input fields
     document.getElementById(`${meal}FoodName`).value = "";
     document.getElementById(`${meal}Calories`).value = "";
   } else {
@@ -63,7 +57,6 @@ function updateTotalCalories(meal) {
   const totalCaloriesElement = document.getElementById(`${meal}TotalCalories`);
   let totalCalories = 0;
 
-  // Get the total calories for the specific meal
   if (meal === 'breakfast') {
     totalCalories = totalCaloriesBreakfast;
   } else if (meal === 'lunch') {
@@ -75,11 +68,9 @@ function updateTotalCalories(meal) {
   totalCaloriesElement.textContent = totalCalories;
 }
 
-// Helper function to simulate :contains selector
 function contains(selector, text) {
   const elements = document.querySelectorAll(selector);
   return Array.from(elements).find(el => el.textContent.includes(text));
 }
 
-// Set the initial state
 changeTab('breakfast');

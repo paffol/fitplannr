8d9d35fc2a9dad7fd0c241760919985bda2914cc
paffol <98435500+paffol@users.noreply.github.com>
2024-01-17 22:21:20 -0500

    start

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+const connect = mongoose.connect("mongodb+srv://Pavel:mongopavel@cluster0.fqokvhb.mongodb.net/fitplanr?retryWrites=true&w=majority");
+
+// Check database connected or not
+connect.then(() => {
+    console.log("Database Connected Successfully");
+})
+.catch(() => {
+    console.log("Database cannot be Connected");
+})
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
